03144613efaae57d3c55fa945db1365f9c728036
Guilherme13Gui13 <20261ctb0100035@estudantes.ifpr.edu.br>
2026-04-07 18:38:20 +0000

    Aula 10 - 1002: area do circulo

--- beecrowd/1002.py
+++ beecrowd/1002.py
@@ -1,11 +1,14 @@
 '''
-Problema 1002 do BeeCrowd
-Guilherme Antunes de Camargo
-2026.04.04
+Problema: beecrowd | 1002
+Data: 2026.04.07
+Estudante: Guilherme Antunes de Camargo
 '''
- # Objetivo calcular a área do círculo
+# Objetivo: calcular a área do cículo a partir da fórmula e do raio e emitir o resultado.
 
-raio = int(input())
-n = 3.14159
-A = raio * raio * n
-print('A=', A)
+# --- ANÁLISE (LIAC) ---
+# Entrada: quais dados o programa recebe e de que tipo?
+# Processamento: quais operações ou cálculos são necessários?
+# Saída: o que deve ser exibido e em qual formato exato?
+
+R = float(input())
+
