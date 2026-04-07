fc73884396c15a89d74620e424b69818e5b6323f
Guilherme13Gui13 <20261ctb0100035@estudantes.ifpr.edu.br>
2026-04-07 17:45:31 +0000

    Aula 10 - 1003: soma simples

--- beecrowd/1003.py
+++ beecrowd/1003.py
@@ -3,13 +3,19 @@ Problema: beecrowd | 1003
 Data: 2026.04.07
 Estudante: Guilherme Antunes de Camargo
 '''
-# Objetivo: descrição sucinta do que o programa deve fazer
+# Objetivo: Somar dois valores inteiro e exibir o resultado.
 
 # --- ANÁLISE (LIAC) ---
-# Entrada: quais dados o programa recebe e de que tipo?
-# Processamento: quais operações ou cálculos são necessários?
-# Saída: o que deve ser exibido e em qual formato exato?
+# Entrada: ele deve receber dois números inteiros, cada um em uma linha diferente.
+# Processamento: deve somar A e B e guardar a informação em SOMA.
+# Saída: deve exibir "SOMA = ", dessa forma
+
+# Leitura das variáveis (observe o enunciado - quantas entradas há?)
 A = int(input())
 B = int(input())
+
+# Cálculo
 SOMA = A + B
+
+# Saída - observe o formato exato no enunciado
 print(f"SOMA = {SOMA}")
