e512ae77d5f9c40eae8a60c37e69db576608ab06
Guilherme13Gui13 <20261ctb0100035@estudantes.ifpr.edu.br>
2026-04-07 18:03:07 +0000

    Aula 10 - 1004: produto simples

--- beecrowd/1004.py
+++ beecrowd/1004.py
@@ -1,13 +1,18 @@
 '''
-Problema 1001 BeeCrowd
-Guilherme Antunes de Camargo
-2026.03.31
+Problema: beecrowd | 1004
+Data: 2026.04.07
+Estudante: Guilherme Antunes de Camargo
 '''
+# Objetivo: multiplicar dois valores inteiros e exibir o resultado.
 
-# Objetivo multiplicar 2 valores
+# --- ANÁLISE (LIAC) ---
+# Entrada: deve receber dois números inteiros, ambos em linhas diferentes.
+# Processamento: multiplicar as variáveis A e B e guardar o resultado em PROD.
+# Saída: deve ser exibido "PROD = ", dessa forma.
 
 A = int(input())
 B = int(input())
 
+# O enunciado pede a operação de multiplicação
 PROD = A * B
 print(f"PROD = {PROD}")
